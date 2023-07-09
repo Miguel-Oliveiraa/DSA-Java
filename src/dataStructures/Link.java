7ed2e4f78e7b4032227f6750c39e93d90b46c7e7
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-07-08 21:37:45 -0300

    Data Structures: Node Link for LinkedBased DS

---
+++ src/dataStructures/Link.java
@@ -0,0 +1,23 @@
+package dataStructures;
+
+/** Node for linkedBased dataStructures **/
+public class Link<E> {
+    // Attributes
+    private E element; // Value of this node
+    private Link<E> next; // Pointer to next node
+
+    // Constructors
+    Link(E it, Link<E> nextVal) {
+        element = it;
+        next = nextVal;
+    }
+    Link(Link<E> nextVal) {
+        next=nextVal;
+    }
+
+    // Methods
+    Link<E> next() { return next; } // Return next field
+    Link<E> setNext(Link<E> nextVal) { return next = nextVal;}  // Set next field
+    E element() { return element; } // Return element
+    E setElement(E it) { return element = it; } // Set element field
+}
