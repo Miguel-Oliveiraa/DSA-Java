6ef29c31536c2ddc1cf4c08b4850e8a1e1c284b2
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-07-11 10:47:19 -0300

    Data Structures: ArrayBased Queue implementation

--- src/dataStructures/ArrayQueue.java
+++ src/dataStructures/ArrayQueue.java
@@ -1,34 +1,37 @@
 package dataStructures;
 
 public class ArrayQueue<E> implements Queue<E>{
+    // Attributes
     private static final int defaultSize = 10;
-    private int maxSize;
-    private int front;
-    private int rear;
-    private E[] listArray;
+    private int maxSize; // Maximum size
+    private int front; // Index of front element
+    private int rear; // Index of rear element
+    private E[] listArray; // Array holding queue
 
+    // Constructor
     ArrayQueue() {this(defaultSize);}
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked") // Generic array
     ArrayQueue(int size) {
-        maxSize = size+1;
+        maxSize = size+1; // One extra space for circular
         rear = 0; front = 1;
-        listArray = (E[])new Object[maxSize];
+        listArray = (E[])new Object[maxSize]; // Create listArray
     }
 
+    // Methods
     public void clear() {
         rear = 0; front = 1;
     }
 
     public void enqueue(E it) {
         assert ((rear+2)%maxSize) != front : "Queue is full";
-        rear = (rear+1)%maxSize;
+        rear = (rear+1)%maxSize; // Circular increment
         listArray[rear] = it;
     }
 
     public E dequeue() {
         assert length() != 0 : "Queue is empty";
         E it = listArray[front];
-        front = (front+1)%maxSize;
+        front = (front+1)%maxSize; // Circular increment
         return it;
     }
 
